26a5b49e61cd51331a051d3a1def4aa8c54133cf
Samuel Trangmar-Keates <sam.keates@groupgti.com>
2016-12-09 17:56:42 +0000

    Adding day5 file

--- day1/day1.py
+++ day1/day1.py
@@ -3,6 +3,9 @@ instructions = f.read().split(", ")
 position = [0, 0] # Start coords
 direction = [1, 0] # NORTH
 
+visited = set([str(position)])
+firstRepeat = None
+
 for instruction in instructions:
     turn = 1 if instruction[:1] == "R" else -1
     distance = int(instruction[1:])
@@ -16,6 +19,12 @@ for instruction in instructions:
     position[0] += (distance * direction[0])
     position[1] += (distance * direction[1])
 
+    if firstRepeat == None:
+        if str(position) in visited:
+            firstRepeat = str(position)
+        else:
+            visited |= set([str(position)])
+
     print distance, "moved, new position is", position
 
-print "final position ", position, "total distance", abs(position[0]) + abs(position[1])
+print "final position ", position, "total distance", abs(position[0]) + abs(position[1]), "first repeat", firstRepeat
